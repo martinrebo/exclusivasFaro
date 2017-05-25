2642af66303e752b93fa2c50b982772b43bcb455
martinrebo <martinrg33@gmail.com>
2017-05-25 17:40:25 -0500

    Structure
    Structure  and content ready

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+      $('.autoplay').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+adaptiveHeight: true,
+        
+});
+    
+  });
